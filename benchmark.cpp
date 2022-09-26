41e6a97158e1df67724a0b6ba251756287eb6139
Sanket Naik <sanketnaik99@gmail.com>
2022-09-25 19:41:06 -0700

    Adds basic and OpenMP vector MM

--- benchmark.cpp
+++ benchmark.cpp
@@ -93,11 +93,15 @@ int main(int argc, char** argv)
 
         // insert start timer code here
 
+        std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
+
         // call the method to do the work
         my_dgemv(n, A, X, Y); 
 
         // insert end timer code here
-
+        std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = end_time - start_time;
+        std::cout << " Elapsed time is : " << elapsed.count() << std::setprecision(8) << " " << std::endl;
 
         // now invoke the cblas method to compute the matrix-vector multiplye
         reference_dgemv(n, Acopy, Xcopy, Ycopy);
--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -14,16 +14,23 @@ const char* dgemv_desc = "OpenMP dgemv.";
 
 void my_dgemv(int n, double* A, double* x, double* y) {
 
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-   }
+   // #pragma omp parallel
+   // {
+   //    int nthreads = omp_get_num_threads();
+   //    int thread_id = omp_get_thread_num();
+   //    printf("Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
+   // }
 
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you may want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
 
+   #pragma omp parallel for
+   for (int i = 0; i < n; i++){
+      #pragma omp parallel for reduction (+:y[i])
+      for (int j = 0; j < n; j++){
+         y[i] += A[ (i*n) + j ] * x[j];
+      }
+   }
 }
 
